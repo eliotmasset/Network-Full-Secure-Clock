1951bf3d88751db1fdcd8e21b1dbee3ba7409b35
Eliot <eliotmasset@gmail.com>
2022-07-04 17:47:13 +0200

    [INIT]

---
+++ clock/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("            --- The Network Full Secure Clock ---");
+    println!("            |                                   |");
+    println!("            |                                   |");
+    println!("            |                                   |");
+    println!("            -------------------------------------");
+}
