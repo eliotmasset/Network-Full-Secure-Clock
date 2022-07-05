7e904ab443eec068f2d333200019b6911ce8371a
Eliot <eliotmasset@gmail.com>
2022-07-05 13:04:54 +0200

    [UPDATE] Communication in tcp

--- clock-client/src/main.rs
+++ clock-client/src/main.rs
@@ -2,22 +2,38 @@
 //Dépendencies 
 extern crate termios;
 use std::io;
-use std::io::Read;
-use std::io::Write;
 use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
+use std::net::{TcpStream, Shutdown};
+use std::io::{Read, Write};
+use std::str::from_utf8;
 
 
 // Main program function
 fn main() {
-    loop {
-        let ret = menu();
-        if ret == 0 {
-            break;
+    match TcpStream::connect("localhost:8080") {
+        Ok(mut stream) => {
+            println!("Successfully connected to server");
+
+            loop {
+                let ret = menu(&stream);
+                if ret == 0 {
+                    break;
+                }
+            }
+            stream.flush().unwrap();
+            stream.write(b"end").unwrap();
+            stream.flush().unwrap();
+            stream.shutdown(Shutdown::Both).unwrap();
+
+        },
+        Err(e) => {
+            println!("Failed to connect to the clock server: {}", e);
         }
-    }    
+    }  
+    println!("GoodBy!");
 }
 
-fn menu() -> i32 {
+fn menu(mut stream: &TcpStream) -> i32 {
     let stdin = 0;
     let termios = Termios::from_fd(stdin).unwrap();
     let mut answer_termios = termios.clone();
@@ -30,28 +46,48 @@ fn menu() -> i32 {
     println!("            ---    The Network Full Secure Clock    ---");
     println!("            | Current Format : YYYY-MM-DD HH:mm:ss    |");
     println!("            | g : Get current time                    |");
-    println!("            | s : Set the timestamp                   |");
+    println!("            | s : Set the pattern timestamp           |");
     println!("            | e : Exit                                |");
     println!("            -------------------------------------------");
-
-    let mut wait : char;
 
     stdout.lock().flush().unwrap();
     reader.read_exact(&mut buffer).unwrap();
     let response = buffer[0] as char;
     tcsetattr(stdin, TCSANOW, & termios).unwrap();
     println!();
-    return ask(response);
+    return ask(response, &stream);
 }
 
-fn ask(c : char) -> i32 {
+fn ask(c : char, mut stream: &TcpStream) -> i32 {
     if c=='e' {
         return 0;
     }
+
     match c {
         'e'=>return 0,
+        'g'=>getTime(&stream),
         _=>println!("Invalid response!"),
     }
+
     println!();
     return 1;
+}
+
+fn getTime(mut stream: &TcpStream) {
+    let msg = b"getTime";
+
+    stream.write(msg).unwrap();
+
+    let mut data = [0; 1024]; // using 6 byte buffer
+    match stream.read(&mut data) {
+        Ok(_) => {
+            let text = from_utf8(&data).unwrap();
+            println!("          Current Time : [ {} ]", text);
+            data = [0; 1024];
+            stream.flush().unwrap();
+        },
+        Err(e) => {
+            println!("Failed to receive data: {}", e);
+        }
+    }
 }
--- clock-server/src/main.rs
+++ clock-server/src/main.rs
@@ -1,19 +1,51 @@
+use std::thread;
+use std::net::{TcpListener, TcpStream, Shutdown};
+use std::io::{Read, Write};
+use std::str::from_utf8;
 
-//Dépendencies 
-use std::io;
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0; 1024]; // using 50 byte buffer
+    'reader: while match stream.read(&mut data) {
+        Ok(_) => {
+            let resp = from_utf8(&data).unwrap().trim_matches(char::from(0));
+            if resp=="getTime" {
+                data = [0; 1024];
+                stream.write(b"10:42").unwrap();
+                stream.flush().unwrap();
+            } else if resp=="end" {
+                data = [0; 1024];
+                stream.flush().unwrap();
+                stream.shutdown(Shutdown::Both).unwrap();
+                break 'reader;
+            }
+            true
+        },
+        Err(_) => {
+            println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            false
+        }
+    } {}
+}
 
-
-// Main program function
 fn main() {
-    println!("            --- The Network Full Secure Clock ---");
-    println!("            |                                   |");
-    println!("            |                                   |");
-    println!("            |                                   |");
-    println!("            -------------------------------------");
-
-    let mut wait = String::new();
-
-    io::stdin()
-        .read_line(&mut wait)
-        .expect("Failed to read line");
+    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("Server listening on port 8080");
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                /* connection failed */
+            }
+        }
+    }
+    // close the socket server
+    drop(listener);
 }
